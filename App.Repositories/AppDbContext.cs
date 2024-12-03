82e8653016f1ac4c0feaae58ca1d193b1eaa5d73
Acar <Acar@ACAR>
2024-12-03 23:15:13 +0300

    Migration

---
+++ App.Repositories/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using App.Repositories.Products;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace App.Repositories;
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; } = default!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
