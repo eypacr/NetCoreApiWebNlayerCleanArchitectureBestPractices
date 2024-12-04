8741e148c613f17c8773849b8fde00f4ba4a047c
Acar <Acar@ACAR>
2024-12-04 15:18:40 +0300

    CreateEndpoint

--- App.Services/ServiceResult.cs
+++ App.Services/ServiceResult.cs
@@ -10,6 +10,7 @@ public class ServiceResult<T>
     [JsonIgnore] public bool IsSuccess => ErrorMessage == null || ErrorMessage.Count == 0;
     [JsonIgnore] public bool IsFail => !IsSuccess;
     [JsonIgnore] public HttpStatusCode Status { get; set; }
+    [JsonIgnore] public string? UrlAsCreated { get; set; }
     //static factory method
     public static ServiceResult<T> Success(T data, HttpStatusCode status = HttpStatusCode.OK)
     {
@@ -19,6 +20,16 @@ public class ServiceResult<T>
             Status = status
         };
     }
+    public static ServiceResult<T> SuccessAsCreated(T data, string urlAsCreated)
+    {
+        return new ServiceResult<T>()
+        {
+            Data = data,
+            Status = HttpStatusCode.Created,
+            UrlAsCreated = urlAsCreated
+        };
+    }
+
     public static ServiceResult<T> Fail(List<string> errorMessage,
             HttpStatusCode status = HttpStatusCode.BadRequest)
     {
