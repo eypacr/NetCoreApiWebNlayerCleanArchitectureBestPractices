38fd44f79869668e89625bb2dccf3c0ee7a71349
Acar <Acar@ACAR>
2024-12-04 12:07:40 +0300

    Product Controller

---
+++ App.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using App.Services.Products;
+using App.Services.Products.Create;
+using App.Services.Products.Update;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.API.Controllers;
+public class ProductsController(IProductService productService) : CustomBaseController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll() => CreateActionResult(await productService.GetAllListAsync());
+
+    [HttpGet("{id:int}")]//https://localhost:5000/api/products?id=2
+    public async Task<IActionResult> GetById(int id) => CreateActionResult(await productService.GetByIdAsync(id));
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProductRequest request)
+    {
+        return CreateActionResult(await productService.CreateAsync(request));
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, UpdateProductRequest request) =>
+            CreateActionResult(await productService.UpdateAsync(id, request));
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id) => CreateActionResult(await productService.DeleteAsync(id));
+}
